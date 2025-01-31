ec1b61a59a752159ffd8fcc668d23beaae325286
M7mod <121365613+mhmod3@users.noreply.github.com>
2025-02-01 00:26:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 import { Telegraf } from 'telegraf';
 import * as aniwatch from 'aniwatch';
 
-console.log(aniwatch); // تحقق من الهيكل لمعرفة كيفية استدعاء Scraper
-
-const HiAnime = aniwatch.default?.HiAnime || aniwatch.HiAnime;
-
-if (!HiAnime) {
-  console.error('لم يتم العثور على HiAnime في aniwatch');
-  process.exit(1);
-}
-
-const hianime = new HiAnime.Scraper();
 const bot = new Telegraf('توكن البوت هنا');
 
 bot.on('text', async (ctx) => {
@@ -19,7 +9,7 @@ bot.on('text', async (ctx) => {
   if (match) {
     const episodeId = match[1];
     try {
-      const data = await hianime.getEpisodeSources(episodeId, 'hd-1', 'sub');
+      const data = await aniwatch.getAnimeEpisodeSources(episodeId);
       ctx.reply(JSON.stringify(data, null, 2));
     } catch (err) {
       ctx.reply('حدث خطأ أثناء جلب البيانات.');
