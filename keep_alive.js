379f3168e6b2bf792696c80e1f137735a93e38cb
M7mod <121365613+mhmod3@users.noreply.github.com>
2025-02-03 03:26:34 +0300

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,15 +1,14 @@
-import express from "express";  // إذا كنت تستخدم import في المشروع
+import http from 'http';
 
-const server = express();
-
-server.all("/", (req, res) => {
-  res.send("Bot is running!");
-});
-
-function keepAlive() {
-  server.listen(3000, () => {
-    console.log("Server is ready.");
+const keepAlive = () => {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot is running');
   });
-}
 
-export default keepAlive;  // استخدام export default
+  server.listen(3001, () => {
+    console.log('Keep-alive server is running on port 3001');
+  });
+};
+
+export default keepAlive;
