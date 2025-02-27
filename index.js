4e08d0c04461bfc9abaa511e1346f8cb63c425fb
M7mod <121365613+mhmod3@users.noreply.github.com>
2025-02-27 21:55:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { HiAnime } from 'aniwatch';
+import HiAnime from "aniwatch";
 
 const hianime = new HiAnime.Scraper();
 
 hianime
-  .getEpisodeSources('steinsgate-3?ep=230', 'hd-1', 'sub')
+  .getEpisodeSources("steinsgate-3?ep=230", "hd-1", "sub")
   .then((data) => console.log(data))
   .catch((err) => console.error(err));
