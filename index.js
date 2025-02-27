ae066085944aaf7ec327286dbd61d790037b07ae
M7mod <121365613+mhmod3@users.noreply.github.com>
2025-02-27 21:59:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import HiAnime from "aniwatch";
+import("aniwatch")
+  .then(({ HiAnime }) => {
+    const hianime = new HiAnime.Scraper();
 
-const hianime = new HiAnime.Scraper();
-
-hianime
-  .getEpisodeSources("steinsgate-3?ep=230", "hd-1", "sub")
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+    hianime
+      .getEpisodeSources("steinsgate-3?ep=230", "hd-1", "sub")
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  })
+  .catch((err) => console.error("Error loading aniwatch:", err));
