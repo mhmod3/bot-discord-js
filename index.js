96ee04183bbf14bcd09e3be6997ba6081b46b109
M7mod <121365613+mhmod3@users.noreply.github.com>
2025-07-04 13:32:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { Telegraf, session, Markup } = require('telegraf');
-const fs = require('fs');
-const path = require('path');
-const AdmZip = require('adm-zip');
-const axios = require('axios');
-const FormData = require('form-data');
-const express = require('express');
+import { Telegraf, session, Markup } from 'telegraf';
+import fs from 'fs';
+import path from 'path';
+import AdmZip from 'adm-zip';
+import axios from 'axios';
+import FormData from 'form-data';
+import express from 'express';
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const DEFAULT_API_TOKEN = process.env.DEFAULT_API_TOKEN;
